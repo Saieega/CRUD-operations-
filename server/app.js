ec6a916203e167fca7eedb8f3a083e9d08e2fa15
Saieega <ravieega880@gmail.com>
2024-08-11 13:30:14 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const alienRouter=require('./routes/aliens')
+
+const url='mongodb://127.0.0.1:27017/CBIT'
+const app=express()
+mongoose.connect(url)
+const con=mongoose.connection
+con.on('open',()=>
+{
+    console.log('connected...')
+})
+app.use(express.json())
+app.use('/aliens',alienRouter)
+app.listen(9000,()=>
+{
+    console.log('server started..')
+})
