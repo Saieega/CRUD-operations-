b84b4fcb39e81e9abccec57d406bf6185a1a15dc
Saieega <ravieega880@gmail.com>
2024-08-19 10:06:12 +0530

    replica

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express=require('express')
 const mongoose=require('mongoose')
 const alienRouter=require('./routes/aliens')
 
-const url='mongodb://127.0.0.1:27017/CBIT'
+// const url='mongodb://127.0.0.1:27017/CBIT'
+const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
 const app=express()
 mongoose.connect(url)
 const con=mongoose.connection
